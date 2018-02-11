885496a11e612d8bac7334b0c00e7f246b69737f
cypherjones <ajjonez@icloud.com>
2018-02-11 16:57:57 -0600

    fixed feedback

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 ### A js Memory Game
 This is going to be a huge challenge for an old duck like me. So here goes. 
+
+##Challenges
+I had a ton of challenges for a first time Frontend guy. I'll try to go through them here in relative detail.
+
+
--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
-// "use strict";
-
-var  $canvas = $('.canvas'),
-       fas = ['superpowers', 'superpowers', 'bicycle', 'bicycle', 'coffee', 'coffee', 's15', 's15', 'btc', 'btc','heart','heart','gamepad','gamepad','creative-commons','creative-commons'],
-       cardMatch = [],
-       attempts = 0;
-       deck = fas.length,
-       $controls = $('.controls')
-       matched = 0,
-       fullDeck = fas.length / 2;
+// game variables
+var $canvas = $('.canvas'),
+    fas = ['superpowers', 'superpowers', 'bicycle', 'bicycle', 'coffee', 'coffee', 's15', 's15', 'btc', 'btc','heart','heart','gamepad','gamepad','creative-commons','creative-commons'],
+    cardMatch = [],
+    attempts = 0;
+    deck = fas.length,
+    $controls = $('.controls')
+    matched = 0,
+    fullDeck = fas.length / 2,
+    flip = 0,
+    firstStar = '';
+// Timer variables
+var pastTimes = [];
+// Timer variables
+var interval,
+		time = 0,
+    hundredsCount = 0,
+		tensCount = 0,
+		secondsCount = 0;
+// Timer flags
+var wasPaused = true,
+		display = $('#display'),
+    hundreds = $('#hundreds'),
+		tens = $('#tens'),
+		seconds = $('#seconds');
 // shuffle the cards
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
@@ -29,7 +44,7 @@ function shuffle(array) {
 }
 
 // this function create a grid
-function makeGrid(num) {
+function makeGameBoard(num) {
 	// let's clean the canvas on every submit
 	$canvas.empty();
 
@@ -47,24 +62,33 @@ function makeGrid(num) {
 	for (let i = 0; i < num ; i++) {
 		$canvas.append('<li class="card"><i class="fa fa-' + fas[i] + '"></i></li>');
 	}
+  // reset stars
+  
+
 	match();
 }; 
 
 // I hope it's not a cheat to use this
 $controls.find('.restart').on('click', function(){
+	// show the alert popup
 	swal({
     allowEscapeKey: false,
     allowOutsideClick: false,
     title: 'Are you sure?',
     text: "Your progress will be Lost!",
     type: 'warning',
+    footer: 'Better hurry, your timer is running...',
     showCancelButton: true,
     confirmButtonColor: '#02ccba',
     cancelButtonColor: '#f95c3c',
-    confirmButtonText: 'Yes, Restart Game!'
-  }).then(function(isConfirm) {
-    if (isConfirm) {
-      makeGrid(deck);
+    confirmButtonText: 'Yes, Restart Game!',
+    onClose: console.log('foo')
+  }).then((result) => {
+    if (result.value) {
+    	//build the game
+      makeGameBoard(deck);
+      // reset the game first
+      resetCallback();      
     }
   })
 })
@@ -72,10 +96,13 @@ $controls.find('.restart').on('click', function(){
 function endGame() {
 
 	if (fullDeck === matched) {
+
+    startCallback();
+
 		swal({
 	    allowEscapeKey: false,
 	    allowOutsideClick: false,
-	    title: 'Nice Job! You did it',
+	    title: `Nice Job! You did it in ${hundredsCount}:${secondsCount}:${tensCount} with only ${attempts} attempts`,
 	    text: "Try again",
 	    type: 'success',
 	    showCancelButton: true,
@@ -84,13 +111,40 @@ function endGame() {
 	    confirmButtonText: 'Let\'s do it!'
 	  }).then(function(isConfirm) {
 	    if (isConfirm) {
-	      makeGrid(deck);
+        // reset the board
+	      makeGameBoard(deck);
+        // clear the timer
+        resetCallback();
+        // reset the stars
+        resetStars();
+        // star the timer
+        startCallback();
 	    }
 	  })
 	} else {
 		console.log('no, we need more. We only have ' + matched + ' And we need ' + fullDeck);
 	}
-}
+};
+
+swal({
+  allowEscapeKey: false,
+  allowOutsideClick: false,
+  title: 'Are you ready to play?',
+  text: "C'mon, you got this!",
+  type: 'info',
+  showCancelButton: true,
+  confirmButtonColor: '#02ccba',
+  cancelButtonColor: '#f95c3c',
+  confirmButtonText: 'Yes, start Game!'
+}).then(function(isConfirm) {
+  if (isConfirm) {
+  	//build the game
+    makeGameBoard(deck);
+    // start the time if it's not already going.
+    startCallback();
+  }
+})
+
 
 function match() {
 	// let's flip the card on the click of the card
@@ -143,21 +197,119 @@ function match() {
 					.removeClass('flip');
 				}, 300)
 			}
-			// let queue up the cardMatche array to catch the cards flipped
+			// let queue up the cardMatch array to catch the cards flipped
 			// and compare them
 			cardMatch = [];
 			// endGame();
 		};
-		attempts++;
-		console.log(attempts);
-		$('.attempts').html(attempts);
+	  // iterate the flips
+    flip++;
+    // add to attempts after 2 flips
+    if (flip % 2) {
+      // count the attempts
+      attempts++;
+      // update the board of attempts
+      $('.attempts').html(attempts);
+      // star taking away stars
+      // ** I think there is a more elegant way to do this, but...
+      if (attempts > '10') {
+        starRemover('1');
+      } 
+      if (attempts > '15') {
+        starRemover('2');
+      }
+      if (attempts > '20') {
+        starRemover('3');
+      }
+    };
 
 		if(fullDeck === matched) {
 			endGame();
 		}
 	})
+	// take out the start btn
+	$('.start').fadeOut(function(){
+		$(this).hide();
+	});
+};
+
+// let's take away some stars after attempts
+function starRemover(starNum) {
+  // loop through stars
+  $('.star').each(function() {
+  // set star var
+  let star = $(this);
+  // take out stars by removing class
+    if (star.attr('data-star') == starNum) {
+      star.removeClass('active');
+    };
+  });
+};
+function resetStars() {
+  $('.star').each(function(){
+    $(this).addClass('active');
+  });
+};
+
+
+// make a timer
+		
+function startCallback(){
+  /**
+   * When the stopwatch starts, the wasPaused flag is set to false, 
+   * time increases. The next time the startButton is clicked, time 
+   * pauses, the wasPaused flag is set to true
+   */
+  if (wasPaused) {
+    clearInterval(interval);
+    interval = setInterval(startTimer, 10);
+    wasPaused = false;
+  } else {
+    clearInterval(interval);
+    wasPaused = true;
+  }
+    
+};
+// reset timer function
+function resetCallback(){
+  clearInterval(interval);
+  wasPaused = true;
+  tensCount = 0;
+  secondsCount = 0;
+  hundredsCount = 0;
+  tens.html("00");
+  seconds.html("00");
+  hundreds.html("00")
+};
+function startTimer() {
+
+  tensCount++;
+  if (tensCount < 10) {
+    tens.html('0' + tensCount);
+  }
+  if (tensCount > 9) {
+    tens.html(tensCount);
+  }
+  if (tensCount > 99) {
+    secondsCount++;
+    seconds.html('0' + secondsCount);
+    tensCount = 0;
+    tens.html('0' + 0);
+  }
+  if (secondsCount > 9) {
+    seconds.html(secondsCount);
+  }
+  if (secondsCount > 59) {
+    hundredsCount++;
+    hundreds.html('0' + hundredsCount);
+    secondsCount = 0;
+    seconds.html('0' + 0);
+  }
+  if (hundredsCount > 9) {
+    hundredsCount.html(hundredsCount)
+  }
 };
 // match();
-makeGrid(deck);
+// makeGrid(deck);
 
 
